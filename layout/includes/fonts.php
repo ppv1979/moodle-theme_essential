ce8f608ae6a67cf28c40a5bc8d8d259187ab6c82
Pavel Pisklakov <pavelpisklakov@Pavels-MacBook-Pro.local>
2013-11-11 21:38:20 +0600

    Cyrilic subset for Google WebFonts
    Adding loading Cyrilic subset for Google Web Fonts, were possible.

--- layout/includes/fonts.php
+++ layout/includes/fonts.php
@@ -1,8 +1,8 @@
 <?php if($PAGE->theme->settings->fontselect ==1) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" type="text/css">
-	<link href="http://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=PT+Sans&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==2) { ?>
-	<link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Cabin" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==3) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Goudy+Bookletter+1911" rel="stylesheet" type="text/css">
@@ -12,7 +12,7 @@
 	<link href="http://fonts.googleapis.com/css?family=Allerta" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==5) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Arvo" rel="stylesheet" type="text/css">
-	<link href="http://fonts.googleapis.com/css?family=PT+Sans" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=PT+Sans&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==6) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Dancing+Script" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Josefin+Sans" rel="stylesheet" type="text/css">
@@ -29,11 +29,11 @@
 	<link href="http://fonts.googleapis.com/css?family=Corben:bold" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Nobile" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==11) { ?>
-	<link href="http://fonts.googleapis.com/css?family=Ubuntu:bold" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Ubuntu:bold&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Vollkorn" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==12) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Bree+Serif" rel="stylesheet" type="text/css">
-	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==13) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Bevan" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Pontano+Sans" rel="stylesheet" type="text/css">
@@ -41,20 +41,20 @@
 	<link href="http://fonts.googleapis.com/css?family=Abril+Fatface" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Average" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==15) { ?>
-	<link href="http://fonts.googleapis.com/css?family=Playfair+Display" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Playfair+Display&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Multi" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==16) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Sansita+One" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Kameron" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==17) { ?>
-	<link href="http://fonts.googleapis.com/css?family=Istok+Web" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Istok+Web&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Lora" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==18) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet" type="text/css">
-	<link href="http://fonts.googleapis.com/css?family=Arimo" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Arimo&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==19) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Nixie+One" rel="stylesheet" type="text/css">
-	<link href="http://fonts.googleapis.com/css?family=Ledger" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Ledger&subset=latin,cyrillic" rel="stylesheet" type="text/css">
 <?php } else if($PAGE->theme->settings->fontselect==20) { ?>
 	<link href="http://fonts.googleapis.com/css?family=Cantata+One" rel="stylesheet" type="text/css">
 	<link href="http://fonts.googleapis.com/css?family=Imprima" rel="stylesheet" type="text/css">
